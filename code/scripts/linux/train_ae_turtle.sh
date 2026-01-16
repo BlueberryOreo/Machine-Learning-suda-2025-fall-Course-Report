131a36b473ffacabfed3c15870f1421889b32875
BlueberryOreo <1761988645@qq.com>
2026-01-16 18:23:42 +0800

    update PCA cluster and turtle leiden resolution

--- code/scripts/linux/train_ae_turtle.sh
+++ code/scripts/linux/train_ae_turtle.sh
@@ -1,3 +1,3 @@
 
-python train.py --config configs/turtle_ae.yaml --out_dir output/turtle_ae
+python train.py --config configs/turtle_ae.yaml --out_dir output/turtle_ae --eval --resume ./output/turtle_ae/turtle_best/model_final.pt
 # --eval --resume ./output/turtle_ae/202601151613/model_final.pt
